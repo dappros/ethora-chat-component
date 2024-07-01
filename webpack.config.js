31d8b2842ed1486244e8005c2b32a338d96c30e0
Roman Leshchuh <roman.leshchuh@dappros.com>
2024-07-01 15:53:49 +0300

    added optimization deps

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,85 @@ module.exports = {
   },
   resolve: {
     extensions: [".ts", ".tsx", ".js", ".jsx"],
+    alias: {
+      "@reduxjs/toolkit": path.resolve(
+        __dirname,
+        "node_modules/@reduxjs/toolkit"
+      ),
+      "@types/react": path.resolve(__dirname, "node_modules/@types/react"),
+      "@types/react-dom": path.resolve(
+        __dirname,
+        "node_modules/@types/react-dom"
+      ),
+      "@types/styled-components": path.resolve(
+        __dirname,
+        "node_modules/@types/styled-components"
+      ),
+      "@xmpp/client": path.resolve(__dirname, "node_modules/@xmpp/client"),
+      axios: path.resolve(__dirname, "node_modules/axios"),
+      ltx: path.resolve(__dirname, "node_modules/ltx"),
+      react: path.resolve(__dirname, "node_modules/react"),
+      "react-dom": path.resolve(__dirname, "node_modules/react-dom"),
+      "react-redux": path.resolve(__dirname, "node_modules/react-redux"),
+      "react-scripts": path.resolve(__dirname, "node_modules/react-scripts"),
+      redux: path.resolve(__dirname, "node_modules/redux"),
+      "redux-saga": path.resolve(__dirname, "node_modules/redux-saga"),
+      "styled-components": path.resolve(
+        __dirname,
+        "node_modules/styled-components"
+      ),
+      uuid: path.resolve(__dirname, "node_modules/uuid"),
+      "@babel/core": path.resolve(__dirname, "node_modules/@babel/core"),
+      "@babel/preset-env": path.resolve(
+        __dirname,
+        "node_modules/@babel/preset-env"
+      ),
+      "@babel/preset-react": path.resolve(
+        __dirname,
+        "node_modules/@babel/preset-react"
+      ),
+      "@babel/preset-typescript": path.resolve(
+        __dirname,
+        "node_modules/@babel/preset-typescript"
+      ),
+      "@types/lodash": path.resolve(__dirname, "node_modules/@types/lodash"),
+      "@types/react-modal": path.resolve(
+        __dirname,
+        "node_modules/@types/react-modal"
+      ),
+      "@types/uuid": path.resolve(__dirname, "node_modules/@types/uuid"),
+      "@types/xmpp__client": path.resolve(
+        __dirname,
+        "node_modules/@types/xmpp__client"
+      ),
+      "babel-loader": path.resolve(__dirname, "node_modules/babel-loader"),
+      "clean-webpack-plugin": path.resolve(
+        __dirname,
+        "node_modules/clean-webpack-plugin"
+      ),
+      "css-loader": path.resolve(__dirname, "node_modules/css-loader"),
+      "css-minimizer-webpack-plugin": path.resolve(
+        __dirname,
+        "node_modules/css-minimizer-webpack-plugin"
+      ),
+      "file-loader": path.resolve(__dirname, "node_modules/file-loader"),
+      "html-webpack-plugin": path.resolve(
+        __dirname,
+        "node_modules/html-webpack-plugin"
+      ),
+      "mini-css-extract-plugin": path.resolve(
+        __dirname,
+        "node_modules/mini-css-extract-plugin"
+      ),
+      "terser-webpack-plugin": path.resolve(
+        __dirname,
+        "node_modules/terser-webpack-plugin"
+      ),
+      "ts-loader": path.resolve(__dirname, "node_modules/ts-loader"),
+      typescript: path.resolve(__dirname, "node_modules/typescript"),
+      webpack: path.resolve(__dirname, "node_modules/webpack"),
+      "webpack-cli": path.resolve(__dirname, "node_modules/webpack-cli"),
+    },
   },
   module: {
     rules: [
