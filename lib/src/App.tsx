33bdf161d59e709259fc18d138d136038ddb87b6
Roman Leshchuh <roman.leshchuh@dappros.com>
2024-07-04 01:32:28 +0300

    fixed named imports

--- lib/src/App.tsx
+++ lib/src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import ReduxWrapper from "./components/MainComponents/ReduxWrapper";
+import { ReduxWrapper } from "./components/MainComponents/ReduxWrapper";
 
 function App() {
   return (
