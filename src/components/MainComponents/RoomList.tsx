92cb0770d0776290c9b79c4f0f65c8beb7c9ae02
Roman Leshchuh <roman.leshchuh@dappros.com>
2024-10-16 16:12:43 +0300

    added config to chatlist

--- src/components/MainComponents/RoomList.tsx
+++ src/components/MainComponents/RoomList.tsx
@@ -10,7 +10,9 @@ import { IRoom } from "../../types/types";
 import { ChatHeaderAvatar } from "./ChatHeaderAvatar";
 import Button from "../styled/Button";
 import { SearchInput } from "../InputComponents/Search";
-import debounce from "lodash/debounce";
+import { useSelector } from "react-redux";
+import { RootState } from "../../roomStore";
+import { getTintedColor } from "../../helpers/getTintedColor";
 
 interface RoomListProps {
   chats: IRoom[];
@@ -30,11 +32,9 @@ const Container = styled.div<{ burgerMenu?: boolean; open?: boolean }>`
           height: 100%;
           transform: ${open ? "translateX(0)" : "translateX(-100%)"};
           transition: transform 0.3s ease-in-out;
-          box-shadow: 2px 0 5px rgba(0, 0, 0, 0.3);
           z-index: 2;
           display: flex;
           flex-direction: column;
-          gap: 8px;
           /* background-color: rgba(156, 104, 104, 0.3); */
           background-color: #fff;
           /* padding: 16px 0px 12px 16px; */
@@ -60,19 +60,19 @@ const BurgerButton = styled.button`
   z-index: 1000;
 `;
 
-const ChatItem = styled.div<{ active: boolean }>`
+const ChatItem = styled.div<{ active: boolean; bg?: string }>`
   display: flex;
   justify-content: space-between;
   border-radius: 16px;
   padding: 10px;
-  border-bottom: 1px solid #ddd;
   cursor: pointer;
-  background-color: ${({ active }) => (active ? "#0052CD" : "#fff")};
+  background-color: ${({ active, bg }) =>
+    active ? (bg ? bg : "#0052CD") : "#fff"};
   color: ${({ active }) => (!active ? "#000" : "#fff")};
 
   &:hover {
-    background-color: ${({ active }) =>
-      active ? "rgba(0, 82, 205, 0.9)" : "rgba(0, 0, 0, 0.05)"};
+    background-color: ${({ active, bg }) =>
+      active ? getTintedColor(bg ? bg : "#0052CD") : "rgba(0, 0, 0, 0.05)"};
   }
 `;
 
@@ -120,6 +120,10 @@ const RoomList: React.FC<RoomListProps> = ({
 }) => {
   const [open, setOpen] = useState(false);
   const [searchTerm, setSearchTerm] = useState("");
+
+  const config = useSelector(
+    (state: RootState) => state.chatSettingStore.config
+  );
 
   const containerRef = useRef<HTMLDivElement>(null);
 
@@ -183,6 +187,7 @@ const RoomList: React.FC<RoomListProps> = ({
                 gap: "16px",
                 alignItems: "center",
                 width: "100%",
+                marginBottom: 8,
               }}
             >
               <SearchInput
@@ -197,6 +202,7 @@ const RoomList: React.FC<RoomListProps> = ({
                 key={index}
                 active={isChatActive(chat)}
                 onClick={() => performClick(chat)}
+                bg={config.colors.primary}
               >
                 <div
                   style={{
