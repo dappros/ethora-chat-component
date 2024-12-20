5358e6918499d34119921f87d24b8dae25b9a65d
Roman Leshchuh <roman.leshchuh@dappros.com>
2024-12-20 14:32:08 +0200

    fix valid date showing

--- src/components/MainComponents/MessageContainer.tsx
+++ src/components/MainComponents/MessageContainer.tsx
@@ -56,7 +56,7 @@ export const MessageContainer: FC<MessageContainerProps> = ({
 
   return (
     <Fragment key={message.id}>
-      {showDateLabel && !activeMessage && message.id === 'delimiter-new' && (
+      {showDateLabel && !activeMessage && message.id !== 'delimiter-new' && (
         <DateLabel date={messageDate} colors={config?.colors} />
       )}
       <MessageComponent message={message} isUser={isUser} isReply={isReply}>
