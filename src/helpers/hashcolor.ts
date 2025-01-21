698aed25184c8d85020ac3cdc10f0b2fed80e31c
Roman Leshchuh <roman.leshchuh@dappros.com>
2025-01-21 16:25:19 +0200

    updated colors for avatars

--- src/helpers/hashcolor.ts
+++ src/helpers/hashcolor.ts
@@ -1,38 +1,34 @@
+import { sha256 } from 'js-sha256';
+
 export function nameToColor(name: string): {
   backgroundColor: string;
-  textColor: string;
 } | null {
-  /**
-   * Generate a consistent background color and readable text color from a string.
-   *
-   * @param name - The string to generate the color for (e.g., username).
-   * @returns An object with `backgroundColor` and `textColor`.
-   */
+  const avatarColors = [
+    '#86d1ee',
+    '#badfff',
+    '#E2F4FB',
+    '#B1E1D9',
+    '#B8F1C4',
+    '#E0FFBE',
+    '#D3C2F1',
+    '#EEE6F9',
+    '#E7BDE6',
+    '#FEBDD1',
+    '#FFEBEE',
+    '#F1DCB6',
+    '#FFE0B6',
+    '#E8E4C9',
+    '#F5F2BC',
+  ];
 
   if (!name) {
-    return { backgroundColor: 'transparent', textColor: '' };
-  }
-  // Generate a hash code from the name
-  let hash = 0;
-  for (let i = 0; i < name.length; i++) {
-    hash = name.charCodeAt(i) + ((hash << 5) - hash);
+    return { backgroundColor: 'transparent' };
   }
 
-  // Convert the hash code to a valid hex background color
-  let backgroundColor = '#';
-  for (let i = 0; i < 3; i++) {
-    const value = (hash >> (i * 8)) & 0xff; // Extract 8 bits
-    backgroundColor += ('00' + value.toString(16)).slice(-2); // Convert to hex
-  }
-
-  // Calculate brightness using the YIQ formula
-  const r = parseInt(backgroundColor.slice(1, 3), 16);
-  const g = parseInt(backgroundColor.slice(3, 5), 16);
-  const b = parseInt(backgroundColor.slice(5, 7), 16);
-  const brightness = (r * 299 + g * 587 + b * 114) / 1000;
-
-  // Use white text for dark backgrounds and black text for light backgrounds
-  const textColor = brightness > 128 ? '#000000' : '#FFFFFF';
-
-  return { backgroundColor, textColor };
+  return {
+    backgroundColor:
+      avatarColors[
+        parseInt(sha256(name).slice(0, 2), 16) % avatarColors.length
+      ],
+  };
 }
