1d52363e705f026d933bc7e360aa6edb554aad49
Dmytro Berberov <d.berberov93@gmail.com>
2025-03-20 17:17:38 +0100

    Deleted .DS_Store and add in .gitignore

--- src/context/xmppProvider.tsx
+++ src/context/xmppProvider.tsx
@@ -25,6 +25,7 @@ const XmppContext = createContext<XmppContextType | null>(null);
 
 interface XmppProviderProps {
   children: ReactNode;
+  config: IConfig;
 }
 
 export const XmppProvider: React.FC<XmppProviderProps> = ({
